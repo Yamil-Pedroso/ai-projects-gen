6d2299b03f66ac420e290b061e49130ac5d3fe84
Yamil Pedroso <yamirovinci@gmail.com>
2025-03-14 15:41:16 +0100

    Modified cors backend

--- project-translate-openai/backend/src/server/server.ts
+++ project-translate-openai/backend/src/server/server.ts
@@ -14,26 +14,34 @@ const PORT = process.env.PORT || 8080;
 
 const app = express();
 
+app.use(cors(
+    {
+        origin: "*",
+        methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+        allowedHeaders: ["Content-Type", "Authorization"],
+        credentials: true,
+    }
+));
 app.use(json());
 app.use(express.urlencoded({ extended: true }));
 
-const allowedOrigins = [
-    process.env.CLIENT_URL || "http://localhost:5173",
-    "https://ai-translator-gen.netlify.app/"
-];
-
-app.use(cors({
-    origin: function (origin, callback) {
-        if (!origin || allowedOrigins.includes(origin)) {
-            callback(null, true);
-        } else {
-            callback(new Error("Not allowed by CORS"));
-        }
-    },
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-    allowedHeaders: ["Content-Type", "Authorization"],
-    credentials: true,
-}));
+//const allowedOrigins = [
+//    process.env.CLIENT_URL || "http://localhost:5173",
+//    "https://ai-translator-gen.netlify.app/"
+//];
+//
+//app.use(cors({
+//    origin: function (origin, callback) {
+//        if (!origin || allowedOrigins.includes(origin)) {
+//            callback(null, true);
+//        } else {
+//            callback(new Error("Not allowed by CORS"));
+//        }
+//    },
+//    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+//    allowedHeaders: ["Content-Type", "Authorization"],
+//    credentials: true,
+//}));
 
 app.use("/api/v1", route);
 
