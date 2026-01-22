a127e5dd0708ff88d3034cade4976a5d6579d729
Yamil Pedroso <yamirovinci@gmail.com>
2026-01-22 09:55:23 +0100

    All projects

--- market-openai/client/src/api/axiosInstace.ts
+++ market-openai/client/src/api/axiosInstace.ts
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  //baseURL: 'https://ai-projects-gen-1.onrender.com/api/v1',
-  baseURL: "http://localhost:8080/api/v1",
+  baseURL: "https://ai-projects-gen.onrender.com/api/v1",
   headers: {
     "Content-Type": "application/json",
   },
